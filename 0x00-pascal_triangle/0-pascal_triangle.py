5366bc91735563d195d5ec3dd09c8b23eeb4e794
babashehu01 <shettimababashehu57@gmail.com>
2023-07-02 23:42:29 +0000

    Solved the pascals triangle challenge

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+'''Defines the pascal's triangle function
+'''
+
+
+def pascal_triangle(n):
+    '''Args:
+        n: number of the rows
+       Return:
+         returns a list of lists of integers(n)
+    '''
+    if n <= 0:
+        return []
+    pascals = []
+
+    for i in range(n):
+        temp = []
+        for j in range(i+1):
+            if j == 0 or j == i:
+                temp.append(1)
+            else:
+                temp.append(pascals[i-1][j-1] + pascals[i-1][j])
+        pascals.append(temp)
+        
+    return pascals
